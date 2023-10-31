c485cd37b3555a1c33e9b979dd15666377ce89ef
Pantonym <221179@virtualwindow.co.za>
2023-10-31 11:21:10 +0200

    New Error card

--- client/src/Components/ErrorCard.js
+++ client/src/Components/ErrorCard.js
@@ -1,10 +1,25 @@
 import React from "react";
+import errImg from "../Assets/Images/Error.png";
+import { Button } from "@mui/material";
 
 function ErrorCard(props) {
+
+    const handleClick = () => {
+        window.location.reload('false');
+    }
+
+    const handleHome = () => {
+        window.location = '/Home';
+    }
+
     return (
         <>
             <div>
-                {props.message}
+                <img src={errImg} alt="Error Image" style={{width: '100%'}}/>
+                <h1>{props.message}</h1>
+                <br></br>
+                <Button variant="contained" onClick={handleClick} style={{marginRight: '25px'}}>Reload Page</Button>
+                <Button variant="contained" onClick={handleHome}>Return Home</Button>
             </div>
         </>
     )
